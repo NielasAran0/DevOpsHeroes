dd47f0a10c4eeaec5115bbff8bfecdd05c0bc426
NielasAran0 <morand.lucas@yahoo.fr>
2023-01-13 15:36:13 +0100

    added fight service (requests not woking, always fallbackmethod)

---
+++ microservices/fight-service/src/main/java/me/escoffier/workshop/supes/Hero.java
@@ -0,0 +1,24 @@
+package me.escoffier.workshop.supes;
+
+import java.util.Random;
+
+
+public class Hero {
+
+    public String name;
+    public String otherName;
+    public int level;
+    public String picture;
+    public String powers;
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                ", name='" + name + '\'' +
+                ", otherName='" + otherName + '\'' +
+                ", level=" + level +
+                ", picture='" + picture + '\'' +
+                ", powers='" + powers + '\'' +
+                '}';
+    }
+}
---
+++ microservices/fight-service/src/main/java/me/escoffier/workshop/supes/Villain.java
@@ -0,0 +1,23 @@
+package me.escoffier.workshop.supes;
+
+import java.util.Random;
+
+public class Villain {
+
+    public String name;
+    public String otherName;
+    public int level;
+    public String picture;
+    public String powers;
+
+    @Override
+    public String toString() {
+        return "Villain{" +
+                ", name='" + name + '\'' +
+                ", otherName='" + otherName + '\'' +
+                ", level=" + level +
+                ", picture='" + picture + '\'' +
+                ", powers='" + powers + '\'' +
+                '}';
+    }
+}
